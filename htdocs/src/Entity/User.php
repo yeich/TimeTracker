e2b66f78510d878e7e667d802c06a569c7ba7e64
Yannick Eich <53116692+yeich@users.noreply.github.com>
2021-08-14 23:34:21 +0200

    User Standardrole

--- htdocs/src/Entity/User.php
+++ htdocs/src/Entity/User.php
@@ -133,7 +133,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLE_DEVELOPER';
 
         return array_unique($roles);
     }
