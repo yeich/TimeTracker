f02d1afe441e8f909237cd00fd05b9be3ea3af19
Yannick Eich <53116692+yeich@users.noreply.github.com>
2021-08-14 21:27:42 +0200

    firewall user

--- htdocs/src/Controller/UserController.php
+++ htdocs/src/Controller/UserController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/user")
+ * @Route("/app/user")
  */
 class UserController extends AbstractController
 {
